5c615d70709a9c685ba2cee1287d6afd9d66900e
en20 <enzolozano90@gmail.com>
2025-03-23 14:24:53 -0300

    Remove unused ThemeProvider import from layout file to streamline code.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Navbar } from "@/components/layout/navbar";
-import { ThemeProvider } from "@/components/layout/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
